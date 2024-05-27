496677950875e3da936195cba593b40193cb0cb5
rohitharavinder <rohitha0112@gmail.com>
2024-05-27 14:04:06 +0000

    ⚡️ changed URL format for gdown

--- web/download.sh
+++ web/download.sh
@@ -3,5 +3,6 @@ pip install gdown
 mkdir -p data
 
 cd data
-gdown https://drive.google.com/file/d/1vvNtObBs_7-5A5ibozHTeIpwJgYOVWxH -O relish_text.jsonl
-gdown https://drive.google.com/file/d/1OYwyZfvVG4ZGCjpLFpj1XH96s2mFyEZ2 -O relish_recoms.jsonl
+
+gdown https://drive.google.com/uc?id=1vvNtObBs_7-5A5ibozHTeIpwJgYOVWxH -O relish_text.jsonl
+gdown https://drive.google.com/uc?id=1OYwyZfvVG4ZGCjpLFpj1XH96s2mFyEZ2 -O relish_recoms.jsonl
